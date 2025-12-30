59100c4ead6ece4f088f0a2b0bf4db3653254fa3
mahan.dev <mahansoodbakhsh@yahoo.com>
2025-12-30 12:00:04 +0330

    storeHelper types

--- helper/storeHelper.ts
+++ helper/storeHelper.ts
@@ -1,6 +1,9 @@
 import { ProductStore, State } from "@/interface/store/store";
 
-const quantityCounter = (state: State, id: number):number => {
+type ProductCalculator = (state: ProductStore[]) => number;
+type QuantityCalculator = (state: State, id: number) => number;
+
+const quantityCounter: QuantityCalculator = (state, id) => {
   const product = state.products.findIndex((item) => item.id === id);
   if (product === -1) return 0;
   else {
@@ -8,11 +11,11 @@ const quantityCounter = (state: State, id: number):number => {
   }
 };
 
-const totalItems = (state: ProductStore[]):number => {
+const totalItems: ProductCalculator = (state) => {
   return state.reduce((acc, cur) => acc + cur.quantity, 0);
 };
 
-const priceHandler = (state: ProductStore[]): number => {
+const priceHandler: ProductCalculator = (state) => {
   return +state
     .reduce((acc, cur) => acc + cur.quantity * cur.price, 0)
     .toFixed(2);
